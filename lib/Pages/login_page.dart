a16cbca338f044efd4a13d122ebed593bd6f5ebb
Jaydeepsinh Devda <djaybhai76@gmail.com>
2023-06-07 09:59:09 +0530

    login form improves

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,50 +1,61 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+   LoginPage({Key? key}) : super(key: key);
+
+   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      child:Column(
-        children: [
-          Image.asset(
-            "assets/images/login.png",
-          fit: BoxFit.cover,
-          height: 300.0),
-          SizedBox(height: 20.0,),
-          Text(
-            "Welcome",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold
-          ),
-          ),
-          SizedBox(height: 20.0,),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    label: Text("Username"),
-                  ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    label: Text("Password"),
-                  ),
-                ),
-                SizedBox(height: 20.0,),
-                ElevatedButton(
-                    onPressed: (){},
-                    child: Text("Log in"))
+      color: Colors.white,
+      child:SingleChildScrollView(
+        child: Form(
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/login1.png",
+              fit: BoxFit.cover,
+              height: 300.0),
+              const SizedBox(height: 20.0,),
+              const Text(
+                "Welcome",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold
+              ),
+              ),
+              const SizedBox(height: 20.0,),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text("Username"),
+                      ),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        label: Text("Password"),
+                      ),
+                    ),
+                    const SizedBox(height: 40.0,),
+                    ElevatedButton(
+                      style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                        onPressed: (){
+                          Navigator.pushNamed(context, "/home");
 
-              ],
-            ),
-          )
-        ],
+                        },
+                        child: const Text("Log in"))
+
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       )
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         "/" : (context) => LoginPage(),
+        "/home" : (context) => HomePage(),
         "/login": (context) => LoginPage()
       },
     );
